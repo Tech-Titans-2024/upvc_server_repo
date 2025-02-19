21b536901a2f7acf73a9c641d6e4ea40c5789735
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-02-19 23:35:52 +0530

    Order Datas Correction

--- routes/PriceList.js
+++ routes/PriceList.js
@@ -2,49 +2,53 @@ const express = require('express');
 const route = express.Router();
 const Product = require('../models/Product');
 
-module.exports = route;
+// ------------------------------------------------------------------------------------------------------- //
+
+// To Fetch Price List Data
 
 route.get('/pricelistdata', async (req, res) => {
     try {
         const priceData = await Product.find();
         res.json(priceData);
-    } catch (err) {
-        res.json("Error");
-    }
+    } 
+    catch (err) { res.json("Error in Find Pricelist Data : ", err) }
 })
 
-//-----------------------------
+// ------------------------------------------------------------------------------------------------------- //
+
+// To Update Price List Data
 
 route.put('/updateprice', async (req, res) => {
+
     const { changedData, price_id } = req.body;
 
     try {
         const updatedPrice = await Product.findOneAndUpdate(
             { price_id: price_id },
             { price: changedData.price },
-            // { new: true }                    
-        );
-        if (updatedPrice) {
-            res.json({ message: "Price Updated Successfully", updatedPrice });
-        } else {
-            res.status(404).json({ message: "Price ID not found" });
-        }
-    } catch (err) {
-        console.error("Error:", err);
+        )
+        if (updatedPrice) { res.json({ message: "Price Updated Successfully", updatedPrice }) } 
+        else { res.status(404).json({ message: "Price ID not found" }) }
+    } 
+    catch (err) {
+        console.error("Error : ", err);
         res.status(500).json({ message: "Internal Server Error" });
     }
 });
-//-------------------
+
+// ------------------------------------------------------------------------------------------------------- //
+
+// To Delete Price List Data
 
 route.put('/deleteprice', async (req, res) => {
+
     const { DeleteId } = req.body;
-    // console.log(DeleteId)
+
     try {
         const destroyPrice = await Product.deleteOne({ price_id: DeleteId })
-        if (destroyPrice) {
-            res.json({ "message": "success" })
-        }
-    } catch (err) {
-        res.json({ "message": "Delete Failed" })
-    }
+        if (destroyPrice) { res.json({ "message": "Success" }) }
+    } 
+    catch (err) { res.json({ "message": "Delete Failed" }) }
 })
+
+module.exports = route;
