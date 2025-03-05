c4c4cd03baeaa26990d299d93d4e92b33992f349
Shafi-Mubaraq <shafimubaraqvkr@gmail.com>
2025-03-05 15:26:07 +0530

    customer details

--- routes/Customer.js
+++ routes/Customer.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const route = express.Router();
-const Product = require('../models/Product');
+const Order = require('../models/Order')
 
+
+route.get('/CustomerDetails', async(req, res)=>{
+    try{
+        const fetchCustomer = await Order.find();
+        console.log(fetchCustomer)
+        res.json(fetchCustomer)
+
+    }
+    catch(error){
+        console.log(error)
+    }
+})
 module.exports = route;
